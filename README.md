e163da16b6e2134ac5736017df208dcf1c6adb7b
2Kelvin <rocketmancodes02@gmail.com>
2023-02-15 10:44:21 +0300

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # synchronizing-with-effects
+
+In this read, I learnt:
+
+- effects let you run some code logic right after react renders in order to synchronize your component with a system outside of react
---
+++ dist/App.js
@@ -0,0 +1,11 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = App;
+function App() {
+  return;
+}
+var root = ReactDOM.createRoot(document.getElementById("rootNode"));
+root.render( /*#__PURE__*/React.createElement(App, null));
